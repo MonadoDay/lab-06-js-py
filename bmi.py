bc538040d3b48870305b91dd034880c004c29bbc
MonadoDay <89417555+MonadoDay@users.noreply.github.com>
2021-10-23 16:52:47 -0400

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,7 @@
+import sys
+weight = input("Enter your weight")
+# print(type(weight))
+height = input("Enter your height")
+# print(type(weight))
+BMI = (float(weight) * 703) / height ^ 2
+# print("Your BMI is " + weight + height + " BMI")
