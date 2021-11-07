afc20c8f58f14fda55edd6f5400e962ae327c2cc
MonadoDay <89417555+MonadoDay@users.noreply.github.com>
2021-11-07 01:34:04 -0400

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -3,5 +3,5 @@ weight = input("Enter your weight")
 # print(type(weight))
 height = input("Enter your height")
 # print(type(weight))
-BMI = (float(weight) * 703) / height ^ 2
+BMI = (float(weight) * 703) / height % 2
 # print("Your BMI is " + weight + height + " BMI")
